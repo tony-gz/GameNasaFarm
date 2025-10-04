7d5425f316855ea4c1cf229ba9f221436965fc6f
Yael-pv10 <yaelpalacios26@gmail.com>
2025-10-04 00:18:29 -0600

    simple modificacion para acomodar las capas de la casa y el arbol

--- js/scenes/BackgroundScene.js
+++ js/scenes/BackgroundScene.js
@@ -37,7 +37,7 @@ class BackgroundScene extends Phaser.Scene {
 
         // 8.2 Árboles de fondo - Uno de cada tipo
         this.createTree(150, height * 0.88, "tree_round", 0.17, 0.4, 1); // Árbol redondo (más lejano)
-        this.createTree(540, height * 0.88, "tree_round", 0.17, 0.4, 1);  // Árbol alto (más cercano)
+        this.createTree(565, height * 0.88, "tree_round", 0.17, 0.4, 1);  // Árbol alto (más cercano)
         
         // 9. TIERRA CON TEXTURA - Directamente después del bosque, sin césped verde
         this.createGroundLayer('grassTiles_ground', height * 0.83, 0.7, 0.07);
@@ -63,6 +63,8 @@ class BackgroundScene extends Phaser.Scene {
 
             // Transparencia
             house.setAlpha(alpha);
+
+            house.setDepth(50)
 
             // Guardar referencia si usas this.layers
             this.layers.push({
@@ -97,7 +99,7 @@ class BackgroundScene extends Phaser.Scene {
         tree.setAlpha(alpha);
 
         // Depth para que esté detrás de otros elementos
-        tree.setDepth(5);
+        tree.setDepth(10);
 
         // Guardar referencia
         this.layers.push({
